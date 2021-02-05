961f8208359fde232c7f12173899a80bd7f63e27
ivan <ivancastilloa@gmail.com>
2021-02-05 13:03:16 +0100

    network status

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { LocalNotifications } from '@ionic-native/local-notifications';
 import { EmailComposer } from '@ionic-native/email-composer';
 import { SocialSharing } from '@ionic-native/social-sharing';
 import { Camera } from '@ionic-native/camera';
+import { Network } from '@ionic-native/network';
 import { DishProvider } from '../providers/dish/dish';
 import { LeaderProvider } from '../providers/leader/leader';
 import { PromotionProvider } from '../providers/promotion/promotion';
@@ -77,7 +78,8 @@ import { RegisterPage } from '../pages/register/register';
     SocialSharing,
     EmailComposer,
     LocalNotifications,
-    Camera
+    Camera,
+    Network
   ]
 })
 export class AppModule {}
